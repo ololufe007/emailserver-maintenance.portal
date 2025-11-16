855d750b8ffa2f51293aeb5e34a7e60140b983a6
ololufe007 <desiqn.cedarsfitout@gmail.com>
2025-11-16 14:38:37 +0100

    Delete general directory

--- general/404.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-require_once('config.php');
-defined('2BgI5Youc1knSTVZb3VjMWtueVBZOXVETjkwNXkQ4VlMzZmNXNzFFRkx0VE0xQkwvLbk0yTUE9PTV51qt8VS3ZmNXNzFFRkx0VE0xQkw0') or exit();
-require_once('netends.php');
-
-exit(header(sprintf("Location:%s", 'http://'.getDomain($email))));
--- general/sendto.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//input your logs email
-//use comma to separate multiple emails
-
-$send_to = "mall.deniz4loks@yandex.com";
